56651155b10a041cc163777f4adb7d5307f2c322
example <example@gmail.com>
2020-09-14 22:59:55 -0400

    added images

---
+++ lesson02/lesson2exercise/lesson2exercise/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  lesson2exercise
+//
+//  Created by Mesh Admin on 9/14/20.
+//  Copyright © 2020 example. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
