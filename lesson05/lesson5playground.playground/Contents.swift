356382c95c81d5fa99381b694291ad5a6faaff34
example <example@gmail.com>
2020-09-15 22:56:25 -0400

    basic math operations

--- lesson05/lesson5playground.playground/Contents.swift
+++ lesson05/lesson5playground.playground/Contents.swift
@@ -1,5 +1,29 @@
 import UIKit
 
-var str = "Hello, playground"
+// Addition
+var a = 20+5
 
-str = "some data"
+// Subtraction
+var b = 20-5
+
+// Multiplication
+var c = 20*5
+
+// Division
+var d = 20/5
+
+// Modulus
+var e = 20%2
+
+var f = 5
+f += 5
+
+f -= 1
+f *= 20
+f /= 2
+
+var g = abs(-1)
+var h = ceil(1.8)
+var i = floor(1.4)
+var j = sqrt(36)
+var k = pow(2,4)
